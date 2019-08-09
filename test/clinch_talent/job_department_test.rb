e18d374a5162925187f4e9b2c1e0527294dad537
steve <steve.quinlan@gmail.com>
2019-08-09 14:39:00 +0100

    Gem bump

--- test/clinch_talent/job_department_test.rb
+++ test/clinch_talent/job_department_test.rb
@@ -8,12 +8,10 @@ class ClinchTalent::JobDepartmentTest < Minitest::Test
   def test_get_signing
     stub_request(:get, 'https://api.clinchtalent.com/v1/job_departments').
       with(:headers => {'Accept' => 'application/vnd.api+json',
-                        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
                         'Authorization' => 'APIAuth access_id:a6qLnFKzp9cObEdcXmKw5zOtLRU=',
                         'Content-Md5' => '1B2M2Y8AsgTpgAmY7PhCfg==',
                         'Content-Type' => 'application/vnd.api+json',
-                        'Date' => 'Wed, 01 Jan 2014 01:01:01 GMT',
-                        'User-Agent' => 'Faraday v0.15.2'})
+                        'Date' => 'Wed, 01 Jan 2014 01:01:01 GMT'})
     ClinchTalent::Base.config('access_id', 'secret')
     ClinchTalent::JobDepartment.all
   end
